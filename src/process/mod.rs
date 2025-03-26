82637a4086b24172de0c203d27659cca5298c3d0
Dominic Pearson <dsp@technoanimal.net>
2025-03-26 23:04:28 +0100

    Refactoring parsers

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -7,10 +7,19 @@
 pub mod command;
 pub mod error;
 pub mod stream;
+pub mod parsers;
 
 pub use command::{Command, CommandBuilder, ProcessHandle};
 pub use error::{ProcessError, ProcessResult};
 pub use stream::{LineStream, RecordStream, StreamParser};
+
+pub use parsers::{
+    CustomParser,
+    DelimitedTextParser,
+    FixedWidthParser,
+    JsonLinesParser,
+    KeyValueParser
+};
 
 /// Trait for sources that provide metrics via external processes
 #[async_trait::async_trait]
